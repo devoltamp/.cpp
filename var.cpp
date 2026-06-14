854df7644789c7f111e7b68a726de9880c7805db
devoltamp <devparmar2306@gmail.com>
2026-06-14 15:56:41 +0530

    declaring the var. & also printf works

---
+++ var.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+/* global var. */
+int a;
+int b = 67;
+
+int main(){
+
+    /* normal printf also
+    works just fine */
+    char ch = 'A';
+    cout << a << endl;
+    cout << b << endl;
+    cout << ch << endl;
+
+    int sum = 3;
+    int num = 3;
+    sum = num + 5;
+    cout << sum << endl;
+    return 0;
+}
