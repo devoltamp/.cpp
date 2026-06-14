014497958db93266067933696f976e38216fb0db
devoltamp <devparmar2306@gmail.com>
2026-06-14 19:12:54 +0530

    method and the manipulator

---
+++ format01.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+
+    float x;
+    int fieldwidth = cout.width();
+    
+    cout.width(100);
+    cout << "hello there!" << endl;
+    cout << fieldwidth << endl;
+
+    /* setw() */
+    cout << setw(10) << 'X' << endl;
+    cout << setw(10) << "hola" << endl;
+
+    puts("enter x: ");
+    cin >> x;
+    cout << setw(10) << internal << x << endl;
+    /* internal seperates the sign */
+
+    return 0;
+}
+
+/* width() -- method always .
+and setw() -- manipulator always <<
+cout.width(10);
+cout << setw(10);
+it'll only work one at a time */
