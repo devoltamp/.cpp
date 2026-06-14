f5c1d10014f764820c9d30ebf0b8a05762095f9c
devoltamp <devparmar2306@gmail.com>
2026-06-14 15:56:10 +0530

    the min and max lim. of the data types

---
+++ limits.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main(){
+
+    cout << "type\t" << "min\t\t" << "max" << endl;
+    cout << "int \t" << INT_MIN << "\t" << INT_MAX << endl;
+    /*
+    .
+    .
+    */
+    cout << endl;
+    cout << sizeof(int) << endl;
+    return 0;
+}
