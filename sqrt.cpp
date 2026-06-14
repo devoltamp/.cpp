de9546d76dd5d08b3c08073d9863e34d51810a7f
devoltamp <devparmar2306@gmail.com>
2026-06-14 16:46:19 +0530

    apn

---
+++ sqrt.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+/* apn -- as per name
+double sqrt(double)
+the declaration is an important part to know */
+
+int main(){
+
+    double a;
+    cin >> a;
+    a = sqrt(a);
+    /* this does not good */
+    cout << a << endl;
+    return 0;
+}
