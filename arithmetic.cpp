a4f58185170922c3076b6965e988f460ae9b969b
devoltamp <devparmar2306@gmail.com>
2026-06-14 22:24:18 +0530

    kid's stuff

---
+++ arithmetic.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int i(2), j(8);
+    /* i like this notation */
+
+    cout << i++ << endl;
+    cout << i << endl;
+    cout << ++i << endl;
+    cout << j-- << endl;
+    cout << j << endl;
+    cout << --j << endl;
+    /* kid's stuff */
+
+    return 0;
+}
+/* variables with multiple
+types could also be evaluated */
