fba7019087d2fa50e46aca24459d40fa8e9d7bae
devoltamp <devparmar2306@gmail.com>
2026-06-14 14:49:48 +0530

    -

---
+++ multi_fn.cpp
@@ -0,0 +1,22 @@
+/* i've got a lot of expereince with c
+so how hard could this be */
+#include <iostream>
+using namespace std;
+
+void line(){
+    cout << "--------------------------------" << endl;
+}
+
+void msg(){
+    cout << "the dragon warrior" << endl;
+}
+
+int main(){
+
+    cout << "hello there!" << endl;
+    line();
+    msg();
+    line();
+    cout << "end" << endl;
+    return 0;
+}
