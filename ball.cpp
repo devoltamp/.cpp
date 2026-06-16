da376a2f0b9c45a16f21478c284e7a82d5bb3c83
devoltamp <devparmar2306@gmail.com>
2026-06-16 09:34:37 +0530

    needed some windows tweaks

--- ball.cpp
+++ ball.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <string>
+// #include "ball.hpp"
+#include <windows.h>
 using namespace std;
 
 #define DELAY 10000000L
 #define CLS (cout << "\033[2J");
 /* i liked this CLS part */
 #define LOCATE(z, s) (cout << "\033[" << z << ';' << s << 'H')
+#define ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x0004
+
 
 int main(){
 
@@ -14,6 +18,13 @@ int main(){
         dx = 1,
         speed = 0;
     string floor(79, '-'), header = "--- jumping ball ---";
+
+    /* the required block */
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD dwMode = 0;
+    GetConsoleMode(hOut, &dwMode);
+    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    SetConsoleMode(hOut, dwMode);
 
     CLS;
     LOCATE(1, 25);
@@ -35,6 +46,7 @@ int main(){
             if (speed == 0) speed = -7;
         }
 
+        Sleep(1);
         speed += 1;
         LOCATE(y, x); cout << ' ';
         y += speed; x += dx;
@@ -43,4 +55,5 @@ int main(){
 }
 
 /* i thought this was going to be a little interactive
--- but i was wrong */
+-- but i was wrong
+edit -- this does work */
