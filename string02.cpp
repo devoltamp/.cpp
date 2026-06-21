0c247065b76818801626ca2ad20ef2898a902754
devoltamp <devparmar2306@gmail.com>
2026-06-21 22:46:26 +0530

    + analogy noice

---
+++ string02.cpp
@@ -0,0 +1,41 @@
+/* concatination is simply done by + */
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+    string prompt = "enter two lines\n",
+    line1, line2, key = "y";
+
+    while(key == "y" || key == "Y"){
+        cout << prompt;
+        getline(cin, line1);
+        getline(cin, line2);
+
+        if (line1 == line2)
+            puts("both same");
+        else{
+            puts("smaller line");
+            cout << ((line1 < line2) ? line1 : line2 ) << endl;
+
+            /* the len. */
+            int len1 = line1.length(),
+                len2 = line2.length();
+
+            if (len1 == len2)
+                puts("both have same length");
+            else{
+                puts("not same");
+            }
+        }
+
+        puts("\nwanna repeat [y/n] ?");
+        do
+            getline(cin, key);
+            /* cause the key is stored as str */
+        while(key != "y" && key != "Y" && key != "n" && key != "N");
+    }
+
+    return 0;
+}
