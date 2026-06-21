f860402710398ec2576ce0d225fd7b6f23e170a6
devoltamp <devparmar2306@gmail.com>
2026-06-22 00:51:05 +0530

    kinda good

--- time.cpp
+++ time.cpp
@@ -4,18 +4,26 @@ it will print 1970 */
 #include <iostream>
 #include <string>
 #include <ctime>
+#include <windows.h>
 using namespace std;
 
 int main(){
 
     long sec;
-    time(&sec);
+    while (1){
 
-    string tm = ctime(&sec);
-    cout << tm;
+        time(&sec);
+        string o_tm = ctime(&sec);
 
-    /* greetings and all could be added
-    via substring
-    -- but i'm not gonna do that */
+        /* greetings and all could be added
+        via substring
+        & also the time and date could be
+        seperate too
+        -- but i'm not gonna do that */
+        o_tm.erase(0, 11);
+        o_tm.erase(9);
+        cout << o_tm << endl << flush;
+        Sleep(1000);
+    }
     return 0;
 }
