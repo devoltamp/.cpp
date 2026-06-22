bb674ef58567c116bb02e5e66a8b3e1ab6f92baf
devoltamp <devparmar2306@gmail.com>
2026-06-22 09:00:03 +0530

    constructor

---
+++ oop03.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class _class{
+public:
+    /* constructor */
+    _class(){
+        cout << "hello there!\n";
+    }
+};
+
+int main(){
+
+    _class obj;
+    return 0;
+}
+
+/* once the obj. is created it'll
+follow up by the exicution */
